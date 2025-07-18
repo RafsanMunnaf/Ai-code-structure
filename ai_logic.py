187321c7af891345a6d166246433bbc2bd609774
rafsan_munnaf <abrafsan21@gmail.com>
2025-07-18 14:41:02 +0600

    commit

--- ai_logic.py
+++ ai_logic.py
@@ -1,15 +1,1 @@
-# The code you will be writing here will be production grade...
-# First write and test the AI logic in local_ai.py as a clean, pure function.
-# Do not handle any request/response or request.data here.
-# Do not use file paths or local files to get data inside the function.
-# Only accept input data as function arguments (json/dict/list etc.).
-# This will be called in a function inside views.py (FBV/CBV) with passing data as function arguments,
-# Always ensure your function is reusable, testable, and side-effect free.
-# use success and error message.
-# from data_format import test_cases
-#
-# for case in test_cases:
-#     func_name = case['function']
-#     args = case['input']
-#     # call function dynamically or via if-else and pass args
-#
+dsds
